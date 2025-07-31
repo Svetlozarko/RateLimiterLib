de56fc306fc9292bc401e9c00c3d28214372943d
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-31 11:25:39 +0300

    Attributes added.

---
+++ RateLimiterLib/Attributes/RateLimitAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RateLimiterLib
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class RateLimitAttribute : Attribute
+    {
+        public int Limit { get; }
+        public int Seconds { get; }
+
+        public RateLimitAttribute(int limit, int seconds)
+        {
+            Limit = limit;
+            Seconds = seconds;
+        }
+    }
+}
